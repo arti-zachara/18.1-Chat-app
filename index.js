f98413c1728518ad0703cf2e2d3688531d630777
Magdalena arti Zachara <magdalena.j.zachara@gmail.com>
2019-06-02 15:19:14 +0200

    Add connect/disconnect/message to index.js

--- index.js
+++ index.js
@@ -13,6 +13,40 @@ app.get("/", (req, res) => {
   res.sendFile(`${__dirname}/index.html`);
 });
 
+// ------ socket.io listening: socket - user connecting
+io.on("connection", socket => {
+  // when client connects (socket)
+  socket.on("join", name => {
+    // user's name is saved on connection to the chat
+    usersService.addUser({
+      id: socket.id,
+      name
+    });
+    // emit update of users list connected to all connected users
+    io.emit("update", {
+      users: usersService.getAllUsers()
+    });
+  });
+
+  // when client disconects from the chat (broadcast - info to the others)
+  socket.on("disconnect", () => {
+    usersService.removeUser(socket.id);
+    socket.broadcast.emit("update", {
+      users: usersService.getAllUsers()
+    });
+  });
+
+  // user sending message emits to all others (broadcast)
+  socket.on("message", message => {
+    // destructurization
+    const { name } = usersService.getUserById(socket.id);
+    socket.broadcast.emit("message", {
+      text: message.text,
+      from: name
+    });
+  });
+});
+
 // --------- server listening ---------------
 server.listen(3000, () => {
   console.log("listening on *:3000");
