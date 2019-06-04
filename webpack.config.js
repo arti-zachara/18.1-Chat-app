e479fc434827c9e61cae13e8f80d3e2caeca4314
Magdalena arti Zachara <magdalena.j.zachara@gmail.com>
2019-06-04 21:20:17 +0200

    Add missing render

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const OptimizeJsPlugin = require("optimize-js-plugin");
 // ----------------------------------------------------------------
 const plugins = [
   new HtmlWebpackPlugin({
-    template: "client/index.html",
+    template: "./client/index.html",
     filename: "index.html",
     inject: "body"
   })
